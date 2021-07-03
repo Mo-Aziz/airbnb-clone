8e1863c111ced2127369d50a1d10a363675d3a4e
MJ <mohamed.aref32@gmail.com>
2021-07-02 19:40:34 -0500

    header compoent with styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
- 
+ import React from 'react';
 import './App.css';
+import Home from "./pages/Home/Home";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+       {/* Home page */}
+       <Header />
+       <Home/>
+          {/* Header  child component*/}
+          {/* Banner child component */}
+          {/* search page  */}
+        {/* cards */}
+        {/* Footer */}
+     
        
     </div>
   );
